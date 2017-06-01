3d49734f5c5975bfbc75580d994105e750421cea
Ali Saeed <asalpha01@gmail.com>
2017-06-01 15:49:08 -0400

    new structure

--- ngram.py
+++ ngram.py
@@ -4,11 +4,21 @@ import csv
 import nltk
 from itertools import zip_longest
 
-# Return n word grams according to the given text
-def get_ngrams(text, n ):
-    n_grams = ngrams(word_tokenize(text), n)
-    return [ ' '.join(grams) for grams in n_grams]
-  
+
+head = ['Incident Ticket Number','Incident Service Type','Original Summary','Stemmed Summary','1-Gram','2-Gram','3-Gram','4-Gram','5-Gram','6-Gram','7-Gram',]
+
+
+# Returns a list of n word grams according to the given text
+def get_ngramlist(text, n ):
+	NgLst = []
+	x = 1
+	while (x <= n):
+		n_grams = ngrams(word_tokenize(text), x)
+		NgLst.append(list([ ' '.join(grams) for grams in n_grams]))
+		x += 1
+	# print(NgLst)
+	return NgLst
+   
 # Extract rows from sheet_data and returns a matrix version containing
 # list of lists
 def extract_rows(sheet_data):
@@ -19,13 +29,53 @@ def extract_rows(sheet_data):
 
 #transposes a list of lists
 def transposed(lists, defval=0):
-   return list(zip_longest(*lists))
+	new =[]
+	x = list(zip_longest(*lists))
+	for tup in x:
+		new.append(list(tup))
+	return new
 
 # writes data in a csv row by row with header head
 def writecsv(head, data, wfile):
 	wfile.writerow(head)
 	for r in data:
 		wfile.writerow(r)
+
+
+def FindGram(p, key, list):
+	match = []
+	for l in list:
+		match.append([s for s in l if key in s])
+	return match
+
+
+
+# def MakeRow(key, prefix, lookup):
+	
+
+
+
+def MakeGram(pre, NGL):
+	all = []
+	# print(NGL[0][0])
+	for OneGram in  NGL[0]:
+		MatchingGrams = FindGram(pre, OneGram, NGL)
+		MakeRow = transposed(MatchingGrams)
+		track = 0
+		for i in MakeRow:
+			if i[0] == None:
+				i[0] = OneGram
+			all.append(pre + i)
+			track += 1
+			# print(i)
+		# print(MakeRow)
+		# all += MakeRow
+	print('----------------')
+	# print(all)
+	return all
+
+
+
 
 # Main Function: reads data from giving csv and creates a new sheet
 # containg 1 to max_ngrams 
@@ -35,23 +85,21 @@ def get_data(input_filename, max_ngram):
 	write_file = open(input_filename + '_ngarms' + '.csv', 'w', newline='', encoding="latin1")
 	csv_write = csv.writer(write_file)
 	sheet_data = extract_rows(csv_read)[1:]
-	header = []
+	# print(sheet_data)
 	new_data = []
-	while (max_ngram > 0):
-		header.append(str(max_ngram) + '_gram')
+	for row in sheet_data:
 		temp = []
-		for row in sheet_data:
-			if (len(row[3].split(' ')) < max_ngram): continue
-			temp += get_ngrams(row[3], max_ngram)
-		new_data.append(temp)
-		max_ngram -= 1
-	writecsv(header, list(transposed(new_data)), csv_write)
+		#temp.append(str(count))
+		temp += row[1:5]
+		NgramList = get_ngramlist(row[4],max_ngram)
+		new_data += MakeGram(temp, NgramList)
+	writecsv(head, new_data, csv_write)
 	read_file.close()
 	write_file.close()
     	
 
 # caller command, contains the path to the source file and max ngram required
-get_data("\\\\officescchome28.office.adroot.bmogc.net\scc28userdata$\\asaee01\home\Python Scripts\Text Analysis\\new", 7)
+get_data("\\\\officescchome28.office.adroot.bmogc.net\scc28userdata$\\asaee01\home\Python Scripts\Text Analysis\\Total", 7)
 
 
 
--- test.py
+++ test.py
@@ -9,16 +9,29 @@ import numpy
 from itertools import zip_longest
 
 l = [[1,2,3,4,5],[6,7,8,9],[11,23,34]]
+x = l[0][1:4]
+some_list = ['asb','abcas','avdsb','asdcabc','asdsdabcasgvfas']
+
+matching = [s for s in some_list if "abc" in s]
+print(matching)
+
+
+
 
 def transposed2(lists, defval=0):
-   return list(zip_longest(*lists))
+	new = []
+	x = list(zip_longest(*lists))
+	for tup in x:
+		new.append(list(tup))
+	return new
 
 
-def a():
-	write_file = open('twqest' + '.csv', 'w', newline='')
-	csv_write = csv.writer(write_file)
-	for r in l:
-		csv_write.writerow(r)
+
+# def a():
+# 	write_file = open('twqest' + '.csv', 'w', newline='')
+# 	csv_write = csv.writer(write_file)
+# 	for r in l:
+# 		csv_write.writerow(r)
 
 # for x in transposed2(l):
 # 	print (x)
@@ -26,7 +39,8 @@ def a():
 
 # print(l[2][1])
 # print('---------')
-# k = list(transposed2(l))
+k = list(transposed2(l))
+print(k)
 # print(k[0])
 # print(k[1])
 # print(k[2])
